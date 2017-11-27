c712456ccf9700e18a3feba90f354be6e3c9ad19
MaxeeZ <33030689+MaxeeZ@users.noreply.github.com>
2017-11-27 17:24:54 +0100

    Interface

---
+++ Interface/src/Fenetre.java
@@ -0,0 +1,26 @@
+import java.awt.Color; 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+ 
+public class Fenetre extends JFrame {
+	
+	public Fenetre(){
+		
+		this.setTitle("LoadRoad");
+		this.setSize(400, 500);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Instanciation d'un objet JPanel
+	    JPanel pan = new JPanel();
+	    //Définition de sa couleur de fond
+	    pan.setBackground(Color.GREEN);        
+	    //On prévient notre JFrame que notre JPanel sera son content pane
+	    this.setContentPane(pan); 
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		Fenetre fen = new Fenetre();
+	}
+	
+}
