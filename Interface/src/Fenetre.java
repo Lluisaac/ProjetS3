49850848f4a0606570603cb9b609a3ed709a97cd
MaxeeZ <33030689+MaxeeZ@users.noreply.github.com>
2018-01-03 15:44:53 +0100

    Interface

--- Interface/src/Fenetre.java
+++ Interface/src/Fenetre.java
@@ -1,30 +1,29 @@
-import java.awt.Color; 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
- 
-public class Fenetre extends JFrame {
-	
-	public Fenetre(){
+
+public class Fenetre extends JFrame{
+
+  public Fenetre(){
 		
-		// Définir le titre de la fenêtre
-		this.setTitle("LoadRoad");
-		// Dimension de la fenêtre
-		this.setSize(400, 500);
-		// Centrer l'appartition de la fenêtre
-		this.setLocationRelativeTo(null);
-		// Fermer la fenêtre sur clique sur croix
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//Instanciation d'un objet JPanel
-	    JPanel pan = new JPanel();
-	    //Définition de sa couleur de fond
-	    pan.setBackground(Color.GREEN);        
-	    //On prévient notre JFrame que notre JPanel sera son content pane
-	    this.setContentPane(pan); 
-		this.setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		Fenetre fen = new Fenetre();
-	}
-	
+    this.setTitle("Box Layout");
+    this.setSize(300, 120);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
+
+    JPanel b2 = new JPanel();
+    //Idem pour cette ligne
+    b2.setLayout(new BoxLayout(b2, BoxLayout.LINE_AXIS));
+    b2.add(new JButton("Lancer Programme"));
+    b2.add(new JButton("Charger Topo"));
+
+    JPanel b4 = new JPanel();
+    //On positionne maintenant ces trois lignes en colonne
+    b4.setLayout(new BoxLayout(b4, BoxLayout.PAGE_AXIS));
+    b4.add(b2);
+		
+    this.getContentPane().add(b4);
+    this.setVisible(true);
+  }
 }
