4d23cc54e5c75e2705de555b27dff98d9bf38dc6
Lluisaac <isaaclluis@gmail.com>
2018-01-06 12:38:58 +0100

    fin

--- Topologie/src/window/Bouton.java
+++ Topologie/src/window/Bouton.java
@@ -1,11 +1,8 @@
 package window;
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
- 
 import javax.swing.JButton;
   
 public class Bouton extends JButton {
