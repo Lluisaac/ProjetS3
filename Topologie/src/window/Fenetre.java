a9d6921c2307b696963e84e13a0304ff485a1f52
Lluisaac <isaaclluis@gmail.com>
2018-01-06 21:24:37 +0100

    Interface presque fin

--- Topologie/src/window/Fenetre.java
+++ Topologie/src/window/Fenetre.java
@@ -2,6 +2,7 @@ package window;
 
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -28,6 +29,15 @@ public class Fenetre extends JFrame {
 												// tout
 	private JTextField cheminFichier = new JTextField(); // JLabel du chemin du
 															// fichier
+	
+	//Les différentes configurations avec des valeurs par défault
+	private JTextField tfConfigChoix1 = new JTextField("5"); //Base
+	private JTextField tfConfigChoix2 = new JTextField("2"); //Creuser
+	private JTextField tfConfigChoix3 = new JTextField("3"); //Remplir
+	private JTextField tfConfigChoix4 = new JTextField("10"); //Pont
+	
+	private int tabValeursConfig[] = new int[4];
+	
 	private Topo topologie = new Topo(128, 128);
 
 	public Fenetre() {
@@ -44,6 +54,7 @@ public class Fenetre extends JFrame {
 		b2.setLayout(new BoxLayout(b2, BoxLayout.LINE_AXIS));
 		b2.add(buttonGenerate);
 		b2.add(buttonChange);
+		b2.add(panelConfigGenerator());
 
 		// On positionne maintenant ces trois lignes en colonne
 		container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
@@ -55,6 +66,64 @@ public class Fenetre extends JFrame {
 
 		this.getContentPane().add(container);
 		this.setVisible(true);
+	}
+
+	private JPanel panelConfigGenerator() {
+		JPanel panelConfig = new JPanel();
+		
+		JPanel panelConfigChoix = new JPanel();
+		panelConfigChoix.setLayout(new GridLayout(2, 2));
+		
+		for (int i = 0; i < 4; i++) {
+			JPanel panelConfigChoixTemp = new JPanel();
+			
+			JLabel labelConfigChoixTemp = new JLabel();
+			
+			switch (i) {
+			case 0:
+				labelConfigChoixTemp.setText("Prix du mètre");
+				panelConfigChoixTemp.add(labelConfigChoixTemp);
+				panelConfigChoixTemp.add(this.tfConfigChoix1);
+				break;
+			case 1:
+				labelConfigChoixTemp.setText("Prix pour creuser");
+				panelConfigChoixTemp.add(labelConfigChoixTemp);
+				panelConfigChoixTemp.add(this.tfConfigChoix2);
+				break;
+			case 2:
+				labelConfigChoixTemp.setText("Prix pour remplir");
+				panelConfigChoixTemp.add(labelConfigChoixTemp);
+				panelConfigChoixTemp.add(this.tfConfigChoix3);
+				break;
+			case 3:
+				labelConfigChoixTemp.setText("Prix d'un mètre de pont");
+				panelConfigChoixTemp.add(labelConfigChoixTemp);
+				panelConfigChoixTemp.add(this.tfConfigChoix4);
+				break;
+			}			
+			panelConfigChoix.add(panelConfigChoixTemp);
+		}
+		
+		panelConfig.setLayout(new GridLayout(2, 1));
+		
+		panelConfig.add(panelConfigChoix);
+		
+		JButton buttonConfig = new JButton("Valider");
+		buttonConfig.addActionListener(new ItemActionValiderConfig());
+		
+		panelConfig.add(buttonConfig);
+		
+		return panelConfig;
+	}
+	
+	class ItemActionValiderConfig implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			tabValeursConfig[0] = Integer.parseInt(tfConfigChoix1.getText());
+			tabValeursConfig[1] = Integer.parseInt(tfConfigChoix2.getText());
+			tabValeursConfig[2] = Integer.parseInt(tfConfigChoix3.getText());
+			tabValeursConfig[3] = Integer.parseInt(tfConfigChoix4.getText());
+		}
 	}
 
 	class ItemActionChange implements ActionListener {
@@ -78,7 +147,7 @@ public class Fenetre extends JFrame {
 			container.repaint();
 		}
 	}
-	
+
 	class ItemActionExecute implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
@@ -89,16 +158,16 @@ public class Fenetre extends JFrame {
 			container.repaint();
 		}
 	}
-	
+
 	public void creerPanelExecute() {
 
 		this.topologie.executerPathfinding();
-		
+
 		// Pour l'ecran d'execution
 		JPanel executePanel = new JPanel();
 		JLabel executeLabel1 = new JLabel("");
 		JLabel executeLabel2 = new JLabel("Résultat:");
-		
+
 		String stringCheminFichier = FileSystemView.getFileSystemView().getRoots()[0].toString() + "\\topo.png";
 
 		if (this.cheminFichier.getText().isEmpty()) {
@@ -114,7 +183,7 @@ public class Fenetre extends JFrame {
 
 		try {
 			myPicture = ImageIO.read(new File(stringCheminFichier));
-			
+
 			BufferedImage scaledImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
 
 			Graphics2D graphics2D = scaledImage.createGraphics();
@@ -122,16 +191,16 @@ public class Fenetre extends JFrame {
 			graphics2D.drawImage(myPicture, 0, 0, 500, 500, null);
 
 			graphics2D.dispose();
-			
+
 			myPicture = scaledImage;
-			
+
 			JLabel generatePicture = new JLabel(new ImageIcon(myPicture));
-			
+
 			executePanel.add(generatePicture);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		container.add(executePanel);
 	}
 
@@ -140,7 +209,7 @@ public class Fenetre extends JFrame {
 		// Pour l'ecran de generation
 		JPanel generatePanel = new JPanel();
 		JLabel generateLabel = new JLabel();
-		
+
 		String stringCheminFichier = FileSystemView.getFileSystemView().getRoots()[0].toString() + "\\topo.png";
 
 		if (this.cheminFichier.getText().isEmpty()) {
@@ -152,7 +221,7 @@ public class Fenetre extends JFrame {
 			stringCheminFichier = this.cheminFichier.getText();
 			generateLabel.setText("Votre image sera modifiée");
 		}
-		
+
 		this.topologie.importerTopologie(stringCheminFichier);
 
 		JButton generateBouton = new JButton("Executer");
@@ -165,7 +234,7 @@ public class Fenetre extends JFrame {
 
 		try {
 			myPicture = ImageIO.read(new File(stringCheminFichier));
-			
+
 			BufferedImage scaledImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
 
 			Graphics2D graphics2D = scaledImage.createGraphics();
@@ -173,33 +242,32 @@ public class Fenetre extends JFrame {
 			graphics2D.drawImage(myPicture, 0, 0, 500, 500, null);
 
 			graphics2D.dispose();
-			
+
 			myPicture = scaledImage;
-			
+
 			JLabel generatePicture = new JLabel(new ImageIcon(myPicture));
-			
+
 			generatePanel.add(generatePicture);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		JPanel boutons = new JPanel();
 
 		if (this.cheminFichier.getText().isEmpty()) {
-			
-			
+
 			JButton retry = new JButton("Re-générer");
-			
+
 			retry.addActionListener(new ItemActionGenerate());
-			
+
 			boutons.add(retry);
 		}
-		
+
 		boutons.add(generateBouton);
 		generatePanel.add(boutons);
 
 		generateBouton.addActionListener(new ItemActionExecute());
-		
+
 		container.add(generatePanel);
 	}
 
